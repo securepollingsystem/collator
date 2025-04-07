105e66b15e25f51c5c9be5fd1416593b0744018f
jerkey <jerkey@g-cipher.net>
2025-04-07 22:04:20 +0000

    log one line per request

--- sloniktest.js
+++ sloniktest.js
@@ -34,19 +34,19 @@ const main = async () => {
 
   app.get('/', (req, res) => {
     res.send('Hello, World!')
-    logAccess(req);
+    logAccess(req,'');
   });
 
   app.get('/opinions', async (req, res) => {
-    logAccess(req);
     var opinions = 'unpopulated';
     var sqlString = 'unpopulated';
     if ( req.query.subset ) {
       sqlString = sql.unsafe`SELECT * FROM sps.opinions WHERE OPINION ILIKE ${req.query.subset}`;
       opinions = await pool.any(sqlString);
-      console.log('safe subset query:',sqlString.values,'returned this many items:',opinions.length);
+      logAccess(req,'Safe subset query: '+sqlString.values+' returned this many items: '+opinions.length);
     } else {
       opinions = await pool.any(sql.unsafe`SELECT * FROM sps.opinions`);
+      logAccess(req,'');
     }
     const stringResponse = JSON.stringify(opinions); // , (key, value) => typeof value === 'bigint' ? value.toString() : value);  // https://github.com/GoogleChromeLabs/jsbi/issues/30
     res.setHeader('Content-Type', 'application/json'); // https://stackoverflow.com/questions/19696240/proper-way-to-return-json-using-node-or-express
@@ -55,7 +55,7 @@ const main = async () => {
 
   app.get('/ipv4', (req, res) => {
     console.log(req);
-    return res.json({ message: `Hello! Your IP address is: ${logAccess(req)}` });
+    return res.json({ message: `Hello! Your IP address is: ${logAccess(req,'')}` });
   });
 
   app.listen(port, () => {
@@ -63,13 +63,13 @@ const main = async () => {
   });
 };
 
-function logAccess(req) {
+function logAccess(req, addlInfo) {
   //const ip = req.ip; // https://stackoverflow.com/questions/29411551/express-js-req-ip-is-returning-ffff127-0-0-1
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; // https://stackoverflow.com/a/39473073
   if (ip.substr(0, 7) == "::ffff:") {
     ip = ip.substr(7)
   }
-  console.log(Date().slice(0,24),ip, 'asks for',req.url,'using',req.headers['user-agent']);
+  console.log(Date().slice(0,24),ip, 'asks for',req.url,'using',req.headers['user-agent'],addlInfo);
   return ip;
 }
 
